81c142c86e471679dca5c0d5eb9f2f297d7401c1
zakordonetss <60739399+zakordonetss@users.noreply.github.com>
2021-01-30 17:32:44 +0200

    created log service and injected it in the counter service

--- src/app/services/counter.service.ts
+++ src/app/services/counter.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from "@angular/core";
+import { LogService } from "./log.service";
 
 @Injectable({
     providedIn: 'root'
 })
 export class CounterService {
+    constructor(private LogService: LogService){}
     count: number = 0;
 
     increase () {
         this.count++
+        this.LogService.log("Increace: " + this.count)
     }
 
     decrease() {
         this.count--
+        this.LogService.log("Decreace: " + this.count)
     }
 }
